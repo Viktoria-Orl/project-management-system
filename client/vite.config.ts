64938f3a95ef9e123ab8196e40ff3e3aadebe9d5
Victoria Orleanova <Vika9_96@mail.ru>
2025-05-20 14:21:57 +0300

    fix: enable Vite hot reload in Docker by exposing host and using polling

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // позволяет принимать внешние подключения
+    watch: {
+      usePolling: true, // чтобы отслеживание файлов работало в Docker volume
+    },
+  },
 });
